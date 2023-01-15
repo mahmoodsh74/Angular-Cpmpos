4b6b25131beada4e0f18000214e48d5f86ab6a9c
Mahmood <mahmoodsh9440@gmail.com>
2023-01-15 22:16:55 +0330

    add home and not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 
 const routes: Routes = [
-
+  {path:'',component:HomeComponent},
+  {path:'**',component:NotFoundComponent}
 ];
 
 @NgModule({

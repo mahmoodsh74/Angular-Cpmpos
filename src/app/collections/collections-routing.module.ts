13b70817cd30d92889342f0325e43314660924bb
Mahmood <mahmoodsh9440@gmail.com>
2023-01-19 20:28:44 +0330

    add bio and companies and partners

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {CollectionsHomeComponent} from "./collections-home/collections-home.component";
+import * as path from "path";
+import {BiographComponent} from "./biograph/biograph.component";
+import {CompaniesComponent} from "./companies/companies.component";
+import {PartnersComponent} from "./partners/partners.component";
 
 const routes: Routes = [
-  {path:'',component:CollectionsHomeComponent}
+  {
+    path: '',
+    component: CollectionsHomeComponent,
+    children: [
+      {path: '', component: BiographComponent},
+      {path: 'companies', component: CompaniesComponent},
+      {path: 'partners', component: PartnersComponent}
+
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CollectionsRoutingModule { }
+export class CollectionsRoutingModule {
+}
